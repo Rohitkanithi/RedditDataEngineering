fda799d2cddcd808498687e86f5446b8818bb9e7
Rohitkanithi <63586900+Rohitkanithi@users.noreply.github.com>
2025-02-21 13:01:15 -0600

    Configured AWS for Airflow S3 integration

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -6,6 +6,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from pipelines.aws_s3_pipeline import upload_s3_pipeline
 from pipelines.reddit_pipeline import reddit_pipeline
 
 default_args = {
@@ -34,3 +35,11 @@ extract = PythonOperator(
     },
     dag=dag
 )
+
+upload_s3 = PythonOperator(
+    task_id = 's3_upload',
+    python_callable = upload_s3_pipeline,
+    dag = dag
+)
+
+extract >> upload_s3
